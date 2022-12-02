f579a62fc5a07e8a40c845b96bfcdf53285f6e08
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2022-12-01 21:42:51 -0300

    Adicionando banco de dados

--- clojure_mysql/src/clojure_mysql/core.clj
+++ clojure_mysql/src/clojure_mysql/core.clj
@@ -1,6 +1,7 @@
 ;;;;;;;;;; Namespace e imports ;;;;;;;;;;
 (ns clojure-mysql.core
   (:require
+   [clojure.java.jdbc :as jdbc]
    [io.pedestal.http :as http]
    [io.pedestal.http.route :as route]))
 
@@ -8,27 +9,45 @@
 (def host "localhost")
 (def port 8899)
 
+(let [db-host "localhost"
+      db-port 5432 
+      db-name "bd"] 
+
+(def db {:classname "com.mysql.cj.jdbc.Driver"
+         :subprotocol "mysql"
+         :subname (str "//" db-host ";" db-port "/" db-name "?useTimezone=true&serverTimezone=UTC")
+         :user "postgres"
+         :password "root"}))
+
+(def selectQuery (jdbc/query db ["SELECT * FROM gerente"]))
+(def insertQuery (jdbc/query db ["INSERT VALUES ('234', 'Bruna', 'baixo', '22') INTO gerente"]))
+(def updateQuery (jdbc/query db ["UPDATE gerente SET nomegerente = 'Bruno' WHERE nomegerente = 'Bruna'"]))
+(def deleteQuery (jdbc/query db ["DELETE * FROM gerente WHERE idgerente = "]))
+
 ;;;;;;;;;; Handlers das rotas ;;;;;;;;;;
 (defn index-handler [request]
   {:status 200
-   :body {:host host
-          :port port}})
+   :body {:port 
+          :host}})
 
 (defn create-handler [request]
+  (println insertQuery)
   {:status 200
-   :body {}})
+   :body "Foi criado um gerente"})
 
 (defn read-handler [request]
   {:status 200
-   :body {}})
+   :body (println selectQuery)})
 
-(defn update-handler [request]
+(defn update-handler [request] 
+  (println updateQuery)
   {:status 200
-   :body {}})
+   :body "O gerente foi atualizado"})
 
-(defn delete-handler [request]
+(defn delete-handler [request] 
+  (println deleteQuery)
   {:status 200
-   :body {}})
+   :body "O gerente foi deletado"})
 
 ;;;;;;;;;; Setup API e servidor ;;;;;;;;;;
 (def routes
